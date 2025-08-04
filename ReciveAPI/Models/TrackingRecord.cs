83f496adb0362a0ba8c523af82f3ab40c056a79e
Ayodhya <ayodhyadeshad22@gmail.com>
2025-08-04 11:03:21 +0530

    Docker

---
+++ ReciveAPI/Models/TrackingRecord.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ReciveAPI.Models
+{
+    public class TrackingRecord
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("TrackingNumber")]
+        public string TrackingNumber { get; set; }
+
+        [BsonElement("JsonObject")]
+        public BsonDocument JsonObject { get; set; }
+
+        [BsonElement("FileName")]
+        public string FileName { get; set; }
+
+        [BsonElement("ProcessedAt")]
+        public DateTime ProcessedAt { get; set; }
+
+    }
+}
